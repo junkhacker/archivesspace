9159f04a862f116ca25ed811a39f18c1833f657d
Manny Rodriguez <manny@theavatar.org>
2018-09-25 17:11:35 -0600

    Frontend UI for ARKs for Archival Objects, and digital object components

--- backend/spec/model_digital_object_component_spec.rb
+++ backend/spec/model_digital_object_component_spec.rb
@@ -11,4 +11,28 @@ describe 'DigitalObjectComponent model' do
     DigitalObjectComponent[doc.id].title.should eq(doc.title)
   end
 
+
+  it "creates an ARK identifier with archival object" do
+    doc = create(:json_digital_object_component)
+
+    doc_row = DigitalObjectComponent.first
+
+    expect(ARKIdentifier.first(:digital_object_component_id => doc_row[:id])).to_not be_nil
+    
+    doc.delete
+  end
+
+  it "deletes ARK Identifier when resource is deleted" do
+    doc = create(:json_digital_object_component)
+
+    doc_row = DigitalObjectComponent.first
+
+    expect(ARKIdentifier.first(:digital_object_component_id => doc_row[:id])).to_not be_nil
+
+    doc.delete
+
+    expect(ARKIdentifier.first(:digital_object_component_id => doc_row[:id])).to be_nil
+  end
+
+
 end
--- common/schemas/digital_object_component.rb
+++ common/schemas/digital_object_component.rb
@@ -51,6 +51,12 @@
           },
 
       "has_unpublished_ancestor" => {"type" => "boolean", "readonly" => "true"},
+      
+      "ark_identifier" => {
+        "type" => "JSONModel(:ark_identifier) object",
+        "readonly" => true,
+        "required" => false
+      }
 
     },
   },
